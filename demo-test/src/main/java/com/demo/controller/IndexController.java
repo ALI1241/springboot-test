39a6b33ff7cda22ff010366634a403669ee73945
ALI <1416978277@qq.com>
2023-04-06 11:23:52 +0800

    版本更新

--- demo-test/src/main/java/com/demo/controller/IndexController.java
+++ demo-test/src/main/java/com/demo/controller/IndexController.java
@@ -5,6 +5,7 @@ import cn.z.id.Id;
 import cn.z.ip2region.Ip2Region;
 import cn.z.phone2region.Phone2Region;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Timestamp;
@@ -41,16 +42,16 @@ public class IndexController {
     /**
      * IP地址
      */
-    @GetMapping("ip")
-    public cn.z.ip2region.Region ip(String ip) {
+    @GetMapping("ip/{ip}")
+    public cn.z.ip2region.Region ip(@PathVariable String ip) {
         return Ip2Region.parse(ip);
     }
 
     /**
      * 手机号码
      */
-    @GetMapping("phone")
-    public cn.z.phone2region.Region phone(String phone) {
+    @GetMapping("phone/{phone}")
+    public cn.z.phone2region.Region phone(@PathVariable String phone) {
         return Phone2Region.parse(phone);
     }
 
